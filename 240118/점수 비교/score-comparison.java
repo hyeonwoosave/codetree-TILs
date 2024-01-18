cc6f8d9c0a9ed78e8931ee436635ee3149ebc35f
hyeonwoosave <150895621+hyeonwoosave@users.noreply.github.com>
2024-01-18 17:56:37 +0900

    Title: 점수 비교, Time: 201ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240118/점수 비교/score-comparison.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int Aa = sc.nextInt();
+        int Ab = sc.nextInt();
+        int Ba = sc.nextInt();
+        int Bb = sc.nextInt();
+
+
+        if (Aa > Ba && Ab > Bb) {
+            System.out.println("1");
+        }
+        else {
+            System.out.println("0");
+        }
+    }
+}
